8ad31a14ce12cdd24764a9c49abe2fd780b9cd23
foodshop1 <[e](mailto:your@email.com)lroyquan@gmail.com>
2025-05-05 19:45:52 -0400

    Updated project with new code

--- script.js
+++ script.js
@@ -20,6 +20,38 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // event listener
   button.addEventListener('click', function () {
-    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+    document.getElementById('home').scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+// about button
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.getElementById('about-button');
+
+  button.addEventListener('click', function () {
+    document.getElementById('about-section').scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+//project button
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.getElementById('projects-button');
+
+  button.addEventListener('click', function () {
+    document.getElementById('projects-section').scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+//contact me section
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.getElementById('contact-button');
+
+  button.addEventListener('click', function () {
+    document.getElementById('contact-section').scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+// options toolbar thingy
+setTimeout(() => {
+  document.getElementById('options').classList.toggle('final');
+}, 3800);
